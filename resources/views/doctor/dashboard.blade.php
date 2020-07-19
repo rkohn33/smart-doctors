23c3b6d693dd6776e98edfaeaa4907ae79a8907d
Zeeshan Habib <zeeshan.habib@builtinsoft.com>
2020-07-19 12:57:45 +0500

    Update

--- resources/views/doctor/dashboard.blade.php
+++ resources/views/doctor/dashboard.blade.php
@@ -1,3 +1,4 @@
+
 @extends('main-layout.master')
 
 @section('content')
@@ -6,94 +7,126 @@
 @include('doctor.includes.side-nav-bar')
 @include('doctor.includes.header')
 
-    <div class="content">
-      <div class="row">
-        <div class="col col-md-8">
-          <div class="welcome pt-5">Welcome Back, Dr John</div>
-          <div class="side-header">Here is your account at a glance.</div>
-          <div class="card card-width card-top">
-            <div class="card-head py-3 px-3">
-              Appointments
-                  <select class="day-dropdown float-right p-1">
-                    <option>Today<img src="{{ url('img/dropdown.svg') }}"></option>
-                    <option>July 1st</option>
-                    <option>July 2nd</option>
-                    <option>July 3rd</option>
-                    <option>July 4th</option>
-                    <option>July 5th</option>
-                  </select>
+<div class="container-fluid">
+    <div class="inner-container hompage">
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <div class="row">
+            <div class="col-lg-8 col-xs-12">
+                <br>
+                <h1 class="top-heading">Welcome Back</h1>
+                <p class="sub-heading">Here is your account at a glance.</p>
             </div>
-
-            <table class="card-table table">
-              <thead>
-                <tr class="table-title">
-                  <th scope="col">NAME</th>
-                  <th scope="col">TYPE</th>
-                  <th scope="col">DATE</th>
-                  <th scope="col">TIME</th>
-                </tr>
-              </thead>
-              <tbody class="table-rows">
-                <tr class="appoint-info">
-                  <td>Mark</td>
-                  <td>Consultation</td>
-                  <td>10/22/2020</td>
-                  <td>10AM</td>
-                </tr>
-                <tr class="appoint-info">
-                  <td>Jacob</td>
-                  <td>Consultation</td>
-                  <td>10/22/2020</td>
-                  <td>10AM</td>
-                </tr>
-                <tr class="appoint-info">
-                  <td>Larry</td>
-                  <td>Consultation</td>
-                  <td>10/22/2020</td>
-                  <td>10AM</td>
-                </tr>
-
-              </tbody>
-
-            </table>
-            <br><br><br><br>
-            <div class="p-4">
-              <button class="next-btn float-right"><span class="pr-2">Next</span><img src="{{ url('img/next.svg') }}"></button>
-
-              <div class="text-center"> <img src="{{ url('img/results.svg') }}">
-                <button class="next-btn float-left"><img src="{{ url('img/prev.svg') }}"><span class="pl-2">Previous</span></button>
-              </div>
+            <div class="col-lg-4  col-xs-12">
+                <img src="images/doctor.jpg" style="width: 100%;height: 100%">
             </div>
-          </div>
         </div>
-        <div class="col col-md-4 doc-img">
-          <div class="mt-200">
-            <div class="card w-18">
-              <div class="card-header">Next consultation</div>
-              <div class="card-body">
-                <h6 class="next-name">Peter Thomas</h6>
-                <p class="next-time">10:30 AM</p>
-                <button class="start-now float-left"><span class="pr-2">Start Now</span>
-                <img src="{{ url('img/white-arrow.svg') }}"></button>
-              </div>
+        <div class="row">
+            <div class="col-lg-8  col-xs-12">
+                <div class="card mb-5">
+                    <div class="card-header">Appointments
+                        <div class="dropdown float-right ">
+                            <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown">
+                                Today
+                            </button>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#"><img src="{{ url('img/dropdown.svg') }}">Today</a>
+                                <a class="dropdown-item" href="#">July 1st</a>
+                                <a class="dropdown-item" href="#">July 2nd</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body" style="padding: 0;">
+                        <div class="table-responsive" style="min-height: 450px;">
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>NAME</th>
+                                    <th>TYPE</th>
+                                    <th>DATE</th>
+                                    <th>TIME</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr class="table-success">
+                                    <td>Peter Thomas</td>
+                                    <td>Consultation</td>
+                                    <td>10/22/2020</td>
+                                    <td>10:30AM</td>
+                                </tr>
+                                <tr>
+                                    <td>Enrique Loss</td>
+                                    <td>Follow-up</td>
+                                    <td>10/22/2020</td>
+                                    <td>11:30AM</td>
+                                </tr>
+                                <tr>
+                                    <td>Marissa Laos</td>
+                                    <td>Follow-up</td>
+                                    <td>10/22/2020</td>
+                                    <td>03:00PM</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="paginate">
+                            <div class="row">
+                                <div class="col-4">
+                                    <button class="btn btn-light disabled"><img src="{{ url('img/prev.svg') }}">Previous</button>
+                                </div>
+                                <div class="col-4 text-center">
+                                    <strong>3</strong> of 3 results
+                                </div>
+                                <div class="col-4">
+                                    <button class="btn btn-light float-right"><img src="{{ url('img/next.svg') }}">Next</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <div class="col-xs-12 col-lg-4">
+                <div class="card mb-5">
+                    <div class="card-header">Next Consultation</div>
+                    <div class="card-body">
+                        <h6><strong>Peter Thomas</strong></h6>
+                        <h3><strong>10:30 AM</strong></h3>
+                        <button class="btn btn-info startnow"><img src="{{ url('img/white-arrow.svg') }}">Start Now &nbsp;<i class="fa fa-angle-right"></i></button>
+                    </div>
+                </div>
+                <div class="card mb-5" style="margin-top: 20px;background: ">
+                    <div class="chart-data">
+                        <div class="row">
+                            <div class="col-8">
+                                <span class="revenue"><strong>$2,503</strong></span>
+                                <i class="revenue-arrow fa fa-arrow-up"></i>
+                                <span class="revenue-percentage"><strong>2%</strong></span>
+                                <h5>Earning this week</h5>
+                            </div>
+                            <div class="col-4">
+                                <div class="dropdown float-right mt-3">
+                                    <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown"><img src="{{ url('img/dropdown.svg') }}">
+                                        Week
+                                    </button>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="#">Link 1</a>
+                                        <a class="dropdown-item" href="#">Link 2</a>
+                                        <a class="dropdown-item" href="#">Link 3</a>
+                                    </div>
+                                </div>
+                                <img src="{{ url('img/card.svg') }}">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body" id="curve_chart" style="width: 100%; height: 200px;padding: 0">
+                    </div>
+                </div>
 
-            <div class="card wallet-card">
-              <div class="p-3">
-                <select class="earnings-btn float-right p-1">
-                  <option>Week <img src="{{ url('img/dropdown.svg') }}"></option>
-                  <option>Month</option>
-                  <option>Quarter</option>
-                </select>
-                <div class="earnings">$2,503</div>
-                <div class="earnings-font">Earnings this week</div>
-              </div>
-              <img src="{{ url('img/card.svg') }}">
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
+
 
 @endsection
+
+
--- resources/views/doctor/profile.blade.php
+++ resources/views/doctor/profile.blade.php
@@ -5,93 +5,100 @@
 <div class="s-layout">
 @include('doctor.includes.side-nav-bar')
 @include('doctor.includes.header')
-<div class="pl-200">
-<div class="profile-banner">
-    <div class="margin-70">
-        <div class="row">
-            <div class="col col-sm-3 avatar-container">
-                <img src="https://420medsthailand.com/wp-content/uploads/2020/05/humberto-chavez-FVh_yqLR9eA-unsplash-881x1024.jpg"
-                    class="rounded float-right profile-avatar" alt="User avatar">
-            </div>
-            <div class="col col-sm-9 avatar-container ">
-                <p class="user-name">{{$profile['salutation'].' '.$profile['first_name'].' '.$profile['last_name']}}</p>
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="sub-header">Specialty</div>
-                        <div class="profile-text">{{$profile['speciality']}}</div>
-                    </div>
-                    <div class="col-lg-4">
-                        <p class="sub-header">Consultation type</p>
-                        <p class="profile-text">{{$profile['consultation_type']}}</p>
 
+<img src="images/gradient.png" class="top-bar-profile">
+<div class="container-fluid">
+    <div class="inner-container profile">
+        <div class="row">
+            <div class="col-lg-3">
+                <img src="images/profile.png" class="profile-photo">
+                <h4><strong>Profile Completeness</strong></h4>
+                <br>
+                <div class="progress">
+                    <div class="progress-bar" style="width:60%;background: #04C6FF"></div>
+                </div>
+                <h3 class="text-center font-weight-bold mt-3">
+                    60%
+                </h3>
+                <h4 class="mt-5 mb-4"><strong>Profile Completeness</strong></h4>
+                <div class="profile-completeness">
+                    <div>
+                        <i class="ion-ios-checkmark-empty"></i>&nbsp;&nbsp;&nbsp;Bio
                     </div>
-                    <div class="col-lg-4">
-                        <p class="sub-header">Website</p>
-                        <p class="profile-text">{{$profile['website']}}</p>
+                    <div>
+                        <i class="ion-ios-checkmark-empty"></i>&nbsp;&nbsp;&nbsp;Hospitals
+                    </div>
+                    <div>
+                        <i class="ion-ios-checkmark-empty"></i>&nbsp;&nbsp;&nbsp;Education
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-9">
+                <div class="profile-right">
+                    <h1 class="profile-heading mt-4">{{$profile['salutation'].' '.$profile['first_name'].' '.$profile['last_name']}}</h1>
+                    <br>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <h5 class="speciality-title"><strong>Speciality</strong></h5>
+                            <p>{{$profile['speciality']}}</p>
+                        </div>
+                        <div class="col-md-4">
+                            <h5 class="speciality-title"><strong>Consultation Type</strong></h5>
+                            <p>{{$profile['consultation_type']}}</p>
+                        </div>
+                        <div class="col-md-4">
+                            <h5 class="speciality-title"><strong>Website</strong></h5>
+                            <p>{{$profile['website']}}</p>
+                        </div>
+                    </div>
+                    <div class="bio mt-5">
+                        <h6 class="font-weight-bold mb-3">BIO</h6>
+                        <p class="mt-3" contenteditable="true">{{$profile['bio']}}</p>
+                        <p class="mt-3" contenteditable="true">{{$profile['bio']}}</p>
+                    </div>
+                    <br>
+                    <hr>
+                    <br>
+                    <div class="hosiptals mt-5">
+                        <h3 class="font-weight-bold mb-3">Hospitals</h3>
+                        <div class="row">
+                            <div class="col-md-4 mb-5">
+                                @php
+                                $hospitals = json_decode($profile['employment'],true);
+                                @endphp
+                                @foreach($hospitals as $hospital)
+                                <h5 class="hospital-title" contenteditable="true">{{$hospital['name']}}</h5>
+                                <p contenteditable="true">{{$hospital['detail']}}</p>
+                                @endforeach
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <br>
+                    <hr>
+                    <div class="hosiptals mt-5">
+                        <h3 class="font-weight-bold mb-3">Education</h3>
+                        <div class="row">
+                            <div class="col-md-4">
+                                @php
+                                $education = json_decode($profile['education'],true);
+                                @endphp
+                                @foreach($education as $edu)
+                                <p>University National Mayor</p>
+                                <h6 class="font-weight-bold">{{$edu['detail']}}</h6>
+                                @endforeach
+                            </div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <div class="row pt-3 margin-70">
-        <div class="col-lg-3 inline">
-            <p class="profile-text1">Profile Completeness</p>
-            <img src="{{ url('img/profile-line.svg') }}" width="200px">
-            <p class="profile-text text-center">60%</p>
-            <p class="profile-text1">Completed Steps</p>
-            <p class="profile-text"> <img src="{{ url('img/profile-circle.svg') }}" height="20px"><span
-                    class="pl-2">Bio</span></p>
-            <p class="profile-text"> <img src="{{ url('img/profile-circle.svg') }}" height="20px"><span
-                    class="pl-2">Hospitals</span>
-            </p>
-            <p class="profile-text"> <img src="{{ url('img/profile-circle.svg') }}" height="20px"><span
-                    class="pl-2">Education</span>
-            </p>
-        </div>
-        <div class="col-lg-9 inline">
-            <p class="sub-header">Bio</p>
-            <p class="profile-text">{{$profile['bio']}}</p>
-        </div>
-    </div>
-
-    <div class="margin-left">
-        <hr>
-        <p class="profile-title">Hospitals</p>
-
-    </div>
-    <div class="row margin-left">
-        @php
-            $hospitals = json_decode($profile['employment'],true);
-        @endphp
-        @foreach($hospitals as $hospital)
-         <div class="col-lg-4">
-             <p class="profile-text">{{$hospital['name']}}</p>
-             <p class="profile-text1">{{$hospital['detail']}}</p>
-             <button type="button" class="btn start-now"><span class="pr-4">Call for info</span>
-             <img src="{{ url('img/white-arrow.svg') }}"></button>
-        </div>
-        @endforeach
-    </div>
-    <br>
-    <div class="margin-left">
-        <hr>
-        <div class="profile-title">Education</div>
-
-    </div>
-    <div class="row margin-left">
-       @php
-            $education = json_decode($profile['education'],true);
-        @endphp
-        @foreach($education as $edu)
-        <div class="col-lg-4">
-            <p class="profile-text">{{$edu['detail']}}</p>
-        </div>
-        @endforeach
-    </div>
-
 </div>
-    </div>
-  </div>
+
+
 
 @endsection
