302f3f792721973a0ed230e36646179c07877708
Zeeshan Habib <zeeshan.habib@builtinsoft.com>
2020-07-22 02:11:40 +0500

    Add meta

--- resources/views/main-layout/master.blade.php
+++ resources/views/main-layout/master.blade.php
@@ -2,6 +2,7 @@
     <head>
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
         <title>Smart Doctors - @yield('title')</title>
 
         <!-- Fonts -->
