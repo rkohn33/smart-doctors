3b4e8e4652ef153e1058a48d2f546e39ab2ab766
unknown <m.zeeshanishaq5@gmail.com>
2020-07-25 03:45:11 +0500

    More design fixes

--- resources/views/doctor/appointment.blade.php
+++ resources/views/doctor/appointment.blade.php
@@ -265,8 +265,6 @@
 
 
 @section('js')
-
-<script src="{{ asset('js/iCalendar.js') }}"></script>
 <script type="text/javascript">
   var iCal = new iCalendar('calendar');
   iCal.render();
@@ -274,37 +272,6 @@
   document.addEventListener('iCalendarDateSelected', function(event) {
   console.log(iCal.selectedDate);
   });
-
-
-$(document).ready(function(){
-    $('#data').after('<div id="nav"></div>');
-    var rowsShown = 4;
-    var rowsTotal = $('#data tbody tr').length;
-    var numPages = rowsTotal/rowsShown;
-    for(i = 0;i < numPages;i++) {
-        var pageNum = i + 1;
-        $('#nav').append('<a href="#" rel="'+i+'">'+pageNum+'</a> ');
-    }
-    $('#data tbody tr').hide();
-    $('#data tbody tr').slice(0, rowsShown).show();
-    $('#nav a:first').addClass('active');
-    $('#nav a').bind('click', function(){
-
-        $('#nav a').removeClass('active');
-        $(this).addClass('active');
-        var currPage = $(this).attr('rel');
-        var startItem = currPage * rowsShown;
-        var endItem = startItem + rowsShown;
-        $('#data tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
-        css('display','table-row').animate({opacity:1}, 300);
-    });
-});
-
-
-
-$(document).ready(function(){
-    $('#myTable').dataTable();
-});
 
 $(document).ready(function(){
   $('#hamburger').click(function(){
